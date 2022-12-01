522a0d8987a0b4e94a31becc909cd4641065b0e1
Chris Allum <ctallum@gmail.com>
2022-12-01 08:53:32 -0500

    Add template files

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+from pygame.locals import *
+
+from display import *
+from maze import *
+from solver import *
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
+
