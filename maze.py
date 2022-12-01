9de87783331cdbdb0c98fb508c506300df60d7b3
Amit Kumar <amitkh@gmail.com>
2022-12-01 12:47:30 -0500

    save and return path

--- maze.py
+++ maze.py
@@ -8,7 +8,7 @@ class Maze():
         self.start = (0,0)
         self.end = (2,2)
 
-    def _getValidMoves(self, cell, visited):
+    def _getValidMoves(self, cell):
         neighbors = []
         for i, j in ADJACENT:
             row = cell[0] + i
@@ -16,26 +16,35 @@ class Maze():
             if row not in range(0, self.end[0]+1) or col not in range(0, self.end[1]+1):
                 continue
 
-            if self.maze[row][col] == 0 and (row, col) not in visited:
+            if self.maze[row][col] == 0:
                 neighbors.append((row, col))
 
         return neighbors
 
     def solve(self):
-        visited = set() 
+        visited = {}
         queue = collections.deque()
         queue.append((0,0))
 
         while queue:
             curr = queue.pop()
-            visited.add(curr)
             if curr == self.end:
                 print("Reached the end")
                 break
 
-            moves = self._getValidMoves(curr, visited)
+            moves = self._getValidMoves(curr)
             for move in moves:
-                queue.append(move)
+                if move not in visited:
+                    queue.append(move)
+                    visited[move] = curr
+
+        path = []
+        path.append(visited[curr])
+        while curr != self.start:
+            path.append(curr := visited[curr])
+
+        path = list(reversed(path))
+        print(path)
 
 if __name__ == '__main__':
     maze = Maze()
