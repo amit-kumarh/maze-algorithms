25083a8ca674d1a8f09607e868a12a0afc33d767
Amit Kumar <amitkh@gmail.com>
2022-12-08 08:51:51 -0500

    mouse progress

--- solver.py
+++ solver.py
@@ -1,23 +1,28 @@
 from maze import Maze
+import matplotlib.pyplot as plt
 import collections
+import random
 
 
-ADJACENT = ((1, 0), (-1, 0), (0, 1), (0, -1))
+ADJACENT = {'N': (1, 0), 'S': (-1, 0), 'E':(0, 1), 'W':(0, -1)}
 def getValidMoves(maze, cell):
-    neighbors = []
-    for i, j in ADJACENT:
+    neighbors = {}
+    dirs = []
+    for d, (i, j) in ADJACENT.items():
         row = cell[0] + i
         col = cell[1] + j
         if row not in range(0, maze.end[0]+1) or col not in range(0, maze.end[1]+1):
             continue
 
         if maze.grid[row][col] == 0:
-            neighbors.append((row, col))
+            neighbors[d] = (row, col)
 
     return neighbors
 
 class Solver:
-    def bfs(self, maze: Maze) -> "list[tuple(int, int)]":
+
+    @staticmethod
+    def bfs(maze: Maze) -> "list[tuple(int, int)]":
         visited = {}
         queue = collections.deque()
         queue.append((0,0))
@@ -25,10 +30,11 @@ class Solver:
         while queue:
             curr = queue.pop()
             if curr == maze.end:
+                print('Reached End')
                 break
 
             moves = getValidMoves(maze, curr)
-            for move in moves:
+            for move in moves.values():
                 if move not in visited:
                     queue.append(move)
                     visited[move] = curr
@@ -42,20 +48,26 @@ class Solver:
         return path
 
     @staticmethod
-    def algo_2(maze: Maze) -> "list[tuple(int, int)]":
+    def mouse(maze: Maze) -> "list[tuple(int, int)]":
+        curr = maze.start
+        dir = None
         path = []
+        path.append(curr)
+        while curr != maze.end:
+            neighbors = getValidMoves(maze, curr)
+            if dir not in neighbors:
+                dir = random.choice(list(neighbors.keys()))
+                curr = neighbors[dir]
+            else:
+                curr = neighbors[dir]
 
-        for x in range(maze.size[0]-1):
-            path.append((x,0))
+            path.append(curr)
+            plt.pcolormesh(maze.grid)
 
-        for y in range(maze.size[1]-1):
-            path.append((maze.size[0]-2,y))
+        print(len(path))
+        return list(set(path))
 
 
-        # fill in function here
-
-        
-        return path
         
     @staticmethod
     def algo_3(maze: Maze) -> "list[tuple(int, int)]":
@@ -73,8 +85,7 @@ class Solver:
     
     algorithms = [
                   ("bfs", bfs.__get__(object)), 
-                  ("algo 2", algo_2.__get__(object)), 
+                  ("mouse", mouse.__get__(object)), 
                   ("algo 3", algo_3.__get__(object)),
                   ]
-
     n_algos = len(algorithms)
